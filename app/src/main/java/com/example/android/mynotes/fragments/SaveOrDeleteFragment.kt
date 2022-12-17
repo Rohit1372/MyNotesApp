2542fe37abe706f8dd5041f3e7b84179e0eb3a3e
Rohit1372 <rohitv1372@gmail.com>
2022-12-17 17:39:26 +0530

    final commit

--- app/src/main/java/com/example/android/mynotes/fragments/SaveOrDeleteFragment.kt
+++ app/src/main/java/com/example/android/mynotes/fragments/SaveOrDeleteFragment.kt
@@ -1,6 +1,6 @@
 package com.example.android.mynotes.fragments
 
-import android.graphics.Color
+import  android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
